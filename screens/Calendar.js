418d14927031c1d68e10bab9391afb2d310dc9f5
Sisi Small <smallsianah@gmail.com>
2023-08-17 12:05:37 -0400

    Added calendar

--- screens/Calendar.js
+++ screens/Calendar.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import { JournalContext } from '../JournalContext';
 
-const Calendar = () => {
+const CalendarScreen = ({ navigation }) => {
+    const { journals } = useContext(JournalContext);
+
+    const onDayPress = (day) => {
+        const journalForDay = journals.find(j => j.day === day.dateString);
+        if (journalForDay) {
+            navigation.navigate('JournalDetail', { journal: journalForDay });
+        }
+    };
+
     return (
-        <View>
-            <Text>Calendar Screen</Text>
+        <View style={styles.container}>
+            <Calendar
+                onDayPress={(day) => onDayPress(day)}
+                markedDates={{
+                    ...journals.reduce((acc, journal) => {
+                        acc[journal.day] = { selected: true };
+                        return acc;
+                    }, {})
+                }}
+            />
         </View>
     );
 }
 
-export default Calendar;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    }
+});
+
+export default CalendarScreen;
+
 
