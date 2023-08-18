57d8176b339c472fd129d8186565a152069b98bd
Sisi Small <smallsianah@gmail.com>
2023-08-17 22:30:10 -0400

    added 'coming soon' text to both clothing and equi

--- screens/Clothing.js
+++ screens/Clothing.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import ClothingBox from '../components/ClothingBox';
+import { View, Text, StyleSheet } from 'react-native';
+// import ClothingBox from '../components/ClothingBox';
 
 const ClothingScreen = () => {
     return (
-        <View>
-            <Text>Coming Soon</Text>
-            {/*<Text>Clothing</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Coming Soon</Text>
+            {/* 
+            <Text>Clothing</Text>
             <ClothingBox itemType="shirts" />
             <ClothingBox itemType="pants" />
             <ClothingBox itemType="shoes" />
-    <ClothingBox itemType="other" />*/}
-            
+            <ClothingBox itemType="other" />
+            */}
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',   // center vertically
+        alignItems: 'center',       // center horizontally
+    },
+    text: {
+        fontSize: 32,              // set font size to 32
+        fontWeight: 'bold',        // set font weight to bold
+    },
+});
+
 export default ClothingScreen;
+
--- screens/Equipment.js
+++ screens/Equipment.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Equipment = () => {
     return (
-        <View>
-            <Text>Coming Soon</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Coming Soon</Text>
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',   // center vertically
+        alignItems: 'center',       // center horizontally
+    },
+    text: {
+        fontSize: 32,              // set font size to 32
+        fontWeight: 'bold',        // set font weight to bold
+    },
+});
+
 export default Equipment;
+
 
