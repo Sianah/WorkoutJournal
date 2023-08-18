cb242187f2b5660bb664260328f901f5b246ec37
Sisi Small <smallsianah@gmail.com>
2023-08-17 23:31:00 -0400

    changed some txt to white

--- screens/WorkoutJournal.js
+++ screens/WorkoutJournal.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { ImageBackground, View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { JournalContext } from '../JournalContext';
 
 const WorkoutJournal = ({ navigation }) => {
@@ -14,30 +14,33 @@ const WorkoutJournal = ({ navigation }) => {
   };
 
   return (
-    
-    <View style={styles.container}>
-      <View style={styles.contentCentered}>
-        <Text style={styles.subtitle}>Workout Journal</Text>
-        <View style={styles.dayContainer}>
-          <Text>Day:</Text>
+    <ImageBackground source={require('./smoke.jpg')} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <View style={styles.contentCentered}>
+          <Text style={styles.subtitle}>Workout Journal</Text>
+          <View style={styles.dayContainer}>
+            <Text style={styles.dayLabel}>Day:</Text>
+            <TextInput
+              placeholder="1"
+              placeholderTextColor="gray"
+              value={day}
+              onChangeText={setDay}
+              style={styles.inputDay}
+            />
+          </View>
           <TextInput
-            placeholder="1"
-            value={day}
-            onChangeText={setDay}
-            style={styles.inputDay}
-          />
+  multiline
+  placeholder="Journal your workout here..."
+  placeholderTextColor="gray" // This will make the placeholder text white
+  value={journalDetail}
+  onChangeText={setJournalDetail}
+  style={styles.journalInput}
+/>
+
+          <Button title="Done" onPress={handleDone} />
         </View>
-        <TextInput
-          multiline
-          placeholder="Journal your workout here..."
-          value={journalDetail}
-          onChangeText={setJournalDetail}
-          style={styles.journalInput}
-        />
-        <Button title="Done" onPress={handleDone} />
       </View>
-    </View>
-    
+    </ImageBackground>
   );
 }
 
@@ -45,12 +48,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 10,
-    paddingTop: 70, // Adjust to match your desired appearance
+    paddingTop: 70, 
   },
   subtitle: {
-    fontSize: 24,
+    fontSize: 32,  
     textAlign: 'center',
     marginBottom: 15,
+    color: 'white', 
   },
   contentCentered: {
     alignItems: 'center',
@@ -60,25 +64,43 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 15,
   },
+  dayLabel: {
+    fontSize: 20,  
+    color: 'white',
+  },
   inputDay: {
+    fontSize: 18,
     marginLeft: 10,
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: '#FFF',
     width: 40,
     textAlign: 'center',
-  },
-  journalInput: {
-    borderWidth: 1,
-    borderColor: '#000',
-    width: '80%',
-    height: 150,
-    padding: 10,
-    marginBottom: 20,
-    textAlignVertical: 'top',
+    color: 'white', // this will make the input text white
+    borderRadius: 5,
+},
+
+journalInput: {
+  fontSize: 18,
+  borderWidth: 1,
+  borderColor: '#FFF',
+  borderRadius:10,
+  width: '80%',
+  height: 150,
+  padding: 10,
+  marginBottom: 20,
+  textAlignVertical: 'top',
+  color: 'white', // this will make the multiline input text white
+},
+
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover", 
+    justifyContent: "center"
   },
 });
 
 export default WorkoutJournal;
+
 
 
 
